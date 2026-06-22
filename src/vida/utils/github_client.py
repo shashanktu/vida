6700eccf90549d51cdd3ce29a0b02c5f8665f120
shashanktu <shashank.tudum@valuemomentum.com>
2026-06-22 11:20:58 +0530

    Update agent-framework dependency to version 1.1.0 and add content_generator.py to sources

--- src/vida/utils/github_client.py
+++ src/vida/utils/github_client.py
@@ -1,7 +1,8 @@
 # from utils.config import hari_github_token as github_token
 from dotenv import load_dotenv
 load_dotenv()
-from vida.utils.config import hari_github_token as github_token
+# from vida.utils.config import hari_github_token as github_token
+from vida.utils.config import github_token as github_token
 from github import Github, Auth #type: ignore
 from vida.utils.request_context import github_pat_ctx
 
@@ -9,6 +10,6 @@ from vida.utils.request_context import github_pat_ctx
 
 def get_github_client(git_token=None):
     git_token = git_token or github_pat_ctx.get(None) or github_token
-    print(git_token)
+    # print(git_token)
     auth = Auth.Token(git_token)
     return Github(auth=auth)
